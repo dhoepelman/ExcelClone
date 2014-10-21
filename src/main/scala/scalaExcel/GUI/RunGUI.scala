a480af0e9cc396d1b34f6d849c4686f27ee39252
Chris Froussios <darkmatter.tm@gmail.com>
2014-10-21 16:46:21 +0200

    ScalaFX HelloStage example

--- src/main/scala/scalaExcel/GUI/RunGUI.scala
+++ src/main/scala/scalaExcel/GUI/RunGUI.scala
@@ -1,34 +1,23 @@
-package scalaExcel.GUI
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
 
-import javafx.application.Application
-import javafx.scene.control.{Label, Button}
-import javafx.scene.{Scene, Group}
-import javafx.scene.layout.{ColumnConstraints, GridPane, StackPane}
-import javafx.scene.paint.Color
-import javafx.scene.shape.Rectangle
-import javafx.stage.Stage
-import javafx.scene.control._
-
-import javafx.collections._
-
-
-class Test extends Application {
-  println("Test()")
-
-  override def start(primaryStage: Stage) {
-    primaryStage.setTitle("Sup!")
-
-    val root = new Group();
-    val s = new Scene(root, 300, 300, Color.IVORY);
-
-    primaryStage.setScene(s)
-    primaryStage.show()
-  }
-}
-
-
-object Test {
-  def main(args: Array[String]) {
-    Application.launch(classOf[Test], args: _*)
+object HelloStageDemo extends JFXApp {
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Hello Stage"
+    width = 600
+    height = 450
+    scene = new Scene {
+      fill = Color.LIGHTGREEN
+      content = new Rectangle {
+        x = 25
+        y = 40
+        width = 100
+        height = 100
+        fill <== when (hover) choose Color.GREEN otherwise Color.RED
+      }
+    }
   }
 }
