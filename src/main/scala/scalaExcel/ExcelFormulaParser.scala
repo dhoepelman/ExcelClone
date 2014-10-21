4bd3ecf07db3023a9dfbadd5afc2ce1b0bca3b8c
David Hoepelman <dhoepelman@gmail.com>
2014-10-21 14:56:20 +0200

    Parser for the Excel formula language No AST yet

--- src/main/scala/scalaExcel/ExcelFormulaParser.scala
+++ src/main/scala/scalaExcel/ExcelFormulaParser.scala
@@ -4,22 +4,56 @@ import java.awt.Label
 
 import scala.util.parsing.combinator._
 
-
+/**
+ * Excel LL grammar based on https://github.com/whatupdave/CilociFormulaEngine/blob/master/Excel.grammar
+ */
 class ExcelFormulaParser extends RegexParsers {
-  def expression    : Parser[Any] = term ~ rep(addop ~ term)
-  def term          : Parser[Any] = factor ~ rep(mulop ~ factor)
-  def factor        : Parser[Any] = number | "(" ~> expression <~ ")" | cellRef | function
+  def StringLiteral : Parser[Any] = """\"(\"\"|[^\"]*)\"""".r
+  def Number        : Parser[Any] = """\d+(\.\d+)?([e][+-]\d{1,3})?""".r
+  def Boolean       : Parser[Any] = """(?i)\Q(true)|(false)\E""".r
 
-  def function      : Parser[Any] = functionName ~ "(" ~> functionArgs <~ ")"
-  def functionArgs  : Parser[Any] = "" | expression ~ rep("," ~ expression)
+  def Cell          : Parser[Any] = """$?[a-z]{1,2}\$?\d{1,5}""".r
+  def CellRange     : Parser[Any] = """\$?[a-z]{1,2}\$?\d{1,5}:\$?[a-z]{1,2}\$?\d{1,5}""".r
+  def RowRange      : Parser[Any] = """\$?\d{1,5}:\$?\d{1,5}""".r
+  def ColumnRange   : Parser[Any] = """\$?[a-z]{1,2}:\$?[a-z]{1,2}""".r
+
+  def FunctionName  : Parser[Any] = """[a-z][\w]*\(""".r
+  def SheetName     : Parser[Any] = """[_a-z][\w]*!""".r
+  def DefinedName   : Parser[Any] = """[_a-z][\w]*""".r
 
   def Formula       : Parser[Any] = "=" ~ Expression
-  def Expression    : Parser[Any] = Primitive | Cell | Function | Label
 
-  def ReferencePrefix : Parser[Any] = Sheet ~ Reference | Reference
+  def Expression    : Parser[Any] = LogicalExpression
 
-  def Reference     : Parser[Any] = Cell
+  def LogicalExpression = repsep(ConcatExpression, LogicalOp)
+  def LogicalOp = "=" | ">" | "<" | ">=" | "<=" | "<>"
+  def ConcatExpression = repsep(AdditiveExpression, "&")
+  def AdditiveExpression = repsep(MultiplicativeExpression, AdditiveOp)
+  def AdditiveOp = "+" | "-"
+  def MultiplicativeExpression = repsep(ExponentationExpression, MultiplicativeOp)
+  def MultiplicativeOp = "*" | "/"
+  def ExponentationExpression = repsep(PercentExpression, "^")
+  def PercentExpression = UnaryExpression ~ """%+""".r
+  def UnaryExpression = UnaryOp ~ BasicExpression
+  def UnaryOp = "+" | "-"
+  def BasicExpression = Primitive | FunctionCall | Reference | ExpressionGroup | ""
 
-  def Sheet         : Parser[Any] = Sheetname ~ "!"
-  def Sheetname     : Parser[Any] = """[a-z0-9]+""".r
+  def Reference = DefinedName | GridReferenceExpression
+  def GridReferenceExpression = SheetName ~ GridReference | GridReference
+  def GridReference = Cell | CellRange | RowRange | ColumnRange
+
+  def FunctionCall = FunctionName ~ Arguments ~ ")"
+  def Arguments = repsep(Expression, ",")
+  def ExpressionGroup = "(" ~> Expression <~ ")"
+  def Primitive = Number | Boolean | StringLiteral | ErrorExpression
+
+  def ErrorExpression = "#DIV/0!" | "#N/A" | "#NAME?" | "#NULL!" | "#REF!" | "#VALUE!" | "#NUM!"
+}
+
+object TestParse {
+  def main(args: Array[String]) {
+    val parser = new ExcelFormulaParser()
+
+    println(parser.parseAll(parser.Number, "1.5"))
+  }
 }
