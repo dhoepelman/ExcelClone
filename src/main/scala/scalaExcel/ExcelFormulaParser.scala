1f15797d0351bca09a2db90a6e202271b3e26620
David Hoepelman <dhoepelman@gmail.com>
2014-10-25 16:11:16 +0200

    Refactored and cleaned up the parser a bit

--- src/main/scala/scalaExcel/ExcelFormulaParser.scala
+++ src/main/scala/scalaExcel/ExcelFormulaParser.scala
@@ -6,10 +6,17 @@ import scala.util.parsing.combinator._
  * Excel LL grammar based on https://github.com/whatupdave/CilociFormulaEngine/blob/master/Excel.grammar
  */
 class ExcelFormulaParser extends RegexParsers {
+  /**
+   * Start parser, this will parse any valid cell value
+   **/
   def Start         : Parser[Expr] = Formula | Bool | Num | Str | Empty
 
+  //****************************
+  //* Literals                 *
+  //****************************
+
   // This String is outside of formula, everything that is not anything else is a string
-  def Str           : Parser[Const]   =  """[^=].*""".r                       ^^ { s => Const(s)}
+  def Str           : Parser[Const]   = """[^=].*""".r                        ^^ { s => Const(s)}
   def Empty         : Parser[Expr]    = ""                                    ^^^ Const("")
 
   // This string literal can be used inside formula's
@@ -17,36 +24,43 @@ class ExcelFormulaParser extends RegexParsers {
   def Num           : Parser[Const]   = """\d+(\.\d+)?([e][+-]\d{1,3})?""".r  ^^ { s => Const(s.toDouble)}
   def Bool          : Parser[Const]   = """(?i)(true)|(false)""".r            ^^ { s => Const(s.toBoolean)}
 
-  // TODO: Generalize the repearted functions of this style. Not trivial due to types
-  //def toNestedBinOps(left : Expr, right : List[String ~ Expr]) : Parser[Expr] = {right.foldLeft(left) { case (l, op ~ r) => BinOp(op, l,r)} }
+  //****************************
+  //* Expressions and formulas *
+  //****************************
 
-  // TODO: In
-  def Expression        : Parser[Expr]= LogicalExpression
+  def BasicExpression   : Parser[Expr]  = Primitive | FunctionCall | Reference | ExpressionGroup
+  def ExpressionGroup   : Parser[Expr]  = "(" ~> Expression <~ ")"
+  def Primitive         : Parser[Expr]  = Num | Bool | StringLiteral | ErrorExpression
 
-  def LogicalExpression : Parser[Expr]= ConcatExpression ~ rep(LogicalOp ~ ConcatExpression) ^^ {case e ~ rest => rest.foldLeft(e) { case (l, op ~ r) => BinOp(op, l,r)} }
-  def LogicalOp         : Parser[Op]  =  """=|(>=)|(<=)|(<>)|>|<""".r ^^ {case "=" => Eq() case ">=" => GTE() case "<=" => LTE() case "<>" => NEq() case ">" => GT() case "<" => LT()}
-
-  def ConcatExpression  : Parser[Expr]= AddExpression ~ rep("&" ~ AddExpression) ^^ {case e ~ rest => rest.foldLeft(e) { case (l, _ ~ r) => BinOp(Concat(), l,r)} }
-
-  def AddExpression     : Parser[Expr]= MultExpression ~ rep(AdditiveOp ~ MultExpression ) ^^ {case e ~ rest => rest.foldLeft(e) { case (l, op ~ r) => BinOp(op, l,r)} }
-  def AdditiveOp        : Parser[Op]  = """\+|\-""".r ^^  {case "+" => Plus() case "-" => Minus()}
-
-  def MultExpression    : Parser[Expr]= ExponentExpression ~ rep(AdditiveOp ~ ExponentExpression) ^^ {case e ~ rest => rest.foldLeft(e) { case (l, op ~ r) => BinOp(op,l,r)} }
-  def MultiplicativeOp  : Parser[Op]  = """\*|\/""".r ^^  {case "*" => Mul() case "/" => Div()}
-
-  def ExponentExpression: Parser[Expr]= PercentExpression ~ rep("^" ~ PercentExpression ) ^^ {case e ~ rest => rest.foldLeft(e) { case (l, _ ~ r) => BinOp(Expon(), l,r)} }
-
-  def PercentExpression : Parser[Expr]= UnaryExpression ~ rep("%") ^^ {case e ~ rest => rest.foldLeft(e) { case (l, _) => UnOp(Percent(), l)}}
-
-  def UnaryExpression   : Parser[Expr]= AdditiveOp.? ~ BasicExpression ^^ {case None ~ e => e case Some(op) ~ e => UnOp(op, e)}
-
-  def Formula           : Parser[Expr]= "=" ~> Expression
+  def Formula           : Parser[Expr]  = "=" ~> Expression
 
   def FunctionName      : Parser[String]= """[a-z][\w]*""".r
-  def FunctionCall      : Parser[Expr]= FunctionName ~ "(" ~ Arguments ~ ")" ^^ {case f ~ _ ~ args ~ _ => Call(f, args)}
+  def FunctionCall      : Parser[Expr]  = FunctionName ~ "(" ~ Arguments ~ ")" ^^ {case f ~ _ ~ args ~ _ => Call(f, args)}
   def Arguments         : Parser[List[Expr]] = repsep(Expression, ",")
-  def ExpressionGroup   : Parser[Expr]= "(" ~> Expression <~ ")"
-  def Primitive = Num | Bool | StringLiteral | ErrorExpression
+
+  def Expression        : Parser[Expr]  = LogicalExpression
+
+  def LogicalExpression : Parser[Expr]  = ConcatExpression ~ rep(LogicalOp ~ ConcatExpression) ^^ toBinOp
+  def LogicalOp         : Parser[Op]    =  """=|(>=)|(<=)|(<>)|>|<""".r ^^ {case "=" => Eq() case ">=" => GTE() case "<=" => LTE() case "<>" => NEq() case ">" => GT() case "<" => LT()}
+
+  def ConcatExpression  : Parser[Expr]  = AddExpression ~ rep(ConcatOp ~ AddExpression) ^^ toBinOp
+  def ConcatOp          : Parser[Op]    = "&" ^^^ Concat()
+
+  def AddExpression     : Parser[Expr]  = MultExpression ~ rep(AdditiveOp ~ MultExpression ) ^^ toBinOp
+  def AdditiveOp        : Parser[Op]    = """\+|\-""".r ^^  {case "+" => Plus() case "-" => Minus()}
+
+  def MultExpression    : Parser[Expr]  = ExponentExpression ~ rep(AdditiveOp ~ ExponentExpression) ^^ toBinOp
+  def MultiplicativeOp  : Parser[Op]    = """\*|\/""".r ^^  {case "*" => Mul() case "/" => Div()}
+
+  def ExponentExpression: Parser[Expr]  = PercentExpression ~ rep(ExponentOp ~ PercentExpression ) ^^ toBinOp
+  def ExponentOp        : Parser[Op]    = "^" ^^^ Expon()
+
+  def PercentExpression : Parser[Expr]  = UnaryExpression ~ rep("%") ^^ {case e ~ rest => rest.foldLeft(e) { case (l, _) => UnOp(Percent(), l)}}
+
+  def UnaryExpression   : Parser[Expr]  = AdditiveOp.? ~ BasicExpression ^^ {case None ~ e => e case Some(op) ~ e => UnOp(op, e)}
+
+  // Transforms a Expr [Op Exprs [Op Expr [...]]] into a BinOp(Op, Expr, BinOp(Op, Expr, ...))
+  def toBinOp(p : Expr ~ List[Op ~ Expr]) = p match { case e ~ rest => rest.foldLeft(e) { case (l, op ~ r) => BinOp(op, l,r) } }
 
   def ErrorExpression   : Parser[Err]= """(?i)(#DIV/0!)|(#N/A)|(#NAME?)|(#NUM!)|(#NULL!)|(#REF!)|(#VALUE!)""".r^^ {case "#DIV/0!" => Err(DivBy0())
   case "#N/A" => Err(NA())
@@ -56,9 +70,11 @@ class ExcelFormulaParser extends RegexParsers {
   case "#REF!" => Err(InvalidRef())
   case "#VALUE!" => Err(InvalidValue())}
 
-  def BasicExpression   : Parser[Expr] = Primitive | FunctionCall | Reference | ExpressionGroup
+  //****************************
+  //* References               *
+  //****************************
 
-  def Reference         : Parser[Expr] = SheetName.? ~ GridReference ^^ {case None ~ e => e case Some(s) ~ e => SheetReference(s, e)}
+  def Reference     : Parser[Expr] = SheetName.? ~ GridReference ^^ {case None ~ e => e case Some(s) ~ e => SheetReference(s, e)}
   def GridReference = Cellref | Cellrange | Rowrange | Columnrange
 
   def SheetName     : Parser[String]     = """[_a-z][\w]*!""".r
@@ -74,11 +90,11 @@ class ExcelFormulaParser extends RegexParsers {
 
 
 }
-
+// TODO: Remove this once the parser is debugged and unit tests are in place
 object TestParse {
   def main(args: Array[String]) {
     val parser = new ExcelFormulaParser()
 
-    println(parser.parseAll(parser.Expression, "1 ^ 5"))
+    println(parser.parseAll(parser.Expression, "1 + 5"))
   }
 }
