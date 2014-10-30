35ace1a8426d5f320152a12edeaa280012219bd4
Arian Stolwijk <stolwijk.arian@gmail.com>
2014-10-30 03:11:31 +0100

    Some more evaluation of BinOp and UnOp expressions

--- src/main/scala/scalaExcel/formula/Evaluator.scala
+++ src/main/scala/scalaExcel/formula/Evaluator.scala
@@ -3,44 +3,85 @@ package scalaExcel.formula
 
 object Evaluator {
 
-  def eval(e: Expr): Value = {
-    e match {
-      case Const(c) => c
-      case Call(f, args) => evalCall(f, args)
-      case BinOp(op, lhs, rhs) => evalBinOp(op, lhs, rhs)
-      case _ => VErr(NA())
-    }
-  }
-
-  def evalBinOp(op: Op, lhs: Expr, rhs: Expr) = op match {
-    case Plus() => reduce(sum, VDouble(0), List(lhs, rhs))
-    case _ => VErr(NA())
-  }
-
-  def evalCall(f: String, args: List[Expr]) = {
-    f match {
-      case "SUM" => reduce(sum, VDouble(0), args)
-      case _ => VErr(NA())
-    }
-  }
-
   def reduce(f: ((Value, Value) => Value), r: Value, args: List[Expr]): Value = r match {
     case e: VErr => e
     case _ => args match {
       case x :: xs => eval(x) match {
         case e: VErr => e
-        case i => reduce(f, f(i, r), xs)
+        case i => reduce(f, f(r, i), xs)
       }
       case _ => r
     }
   }
 
-  def sum(lhs: Value, rhs: Value) = lhs match {
-    case VDouble(l) => rhs match {
-      case VDouble(r) => VDouble(l + r)
-      case _ => VErr(NotNumeric())
-    }
+  def applyToDouble(f: (Double => Double))(v: Value) = v match {
+    case e: VErr => e
+    case VDouble(d) => VDouble(f(d))
     case _ => VErr(NotNumeric())
+  }
+
+  def applyToDoubles(f: (Double, Double) => Double)(lhs: Value, rhs: Value) = (lhs, rhs) match {
+    case (VDouble(l), VDouble(r)) => VDouble(f(l, r))
+    case es => pickError(es, NotNumeric())
+  }
+
+  def eval(e: Expr): Value = {
+    e match {
+      case Const(c) => c
+      case Call(f, args) => evalCall(f, args)
+      case BinOp(op, lhs, rhs) => evalBinOp(op, lhs, rhs)
+      case UnOp(op, v) => evalUnOp(op, v)
+      case _ => VErr(NA())
+    }
+  }
+
+  def evalCall(f: String, args: List[Expr]) = {
+    f match {
+      case "SUM" => reduce(applyToDoubles(_ + _), VDouble(0), args)
+      case _ => VErr(NA())
+    }
+  }
+
+  def evalBinOp(op: Op, lhs: Expr, rhs: Expr) = op match {
+    case Eq()     => VBool(false)
+    case GT()     => VBool(false)
+    case LT()     => VBool(false)
+    case GTE()    => VBool(false)
+    case LTE()    => VBool(false)
+    case NEq()    => VBool(false)
+    case Concat() => reduce(concat, VString(""), List(lhs, rhs))
+    case Plus()   => reduce(applyToDoubles(_ + _), VDouble(0), List(lhs, rhs))
+    case Minus()  => VDouble(0)
+    case Mul()    => reduce(applyToDoubles(_ * _), VDouble(1), List(lhs, rhs))
+    case Div()    => VDouble(0)
+    case Expon()  => VDouble(0)
+    case _ => VErr(NA())
+  }
+
+  def evalUnOp(op: Op, v: Expr) = op match {
+    case Plus()    => applyToDouble(+ _)(eval(v))
+    case Minus()   => applyToDouble(- _)(eval(v))
+    case Percent() => applyToDouble(_ / 100)(eval(v))
+    case _ => VErr(NA())
+  }
+
+  def concat(lhs: Value, rhs: Value): Value = (lhs, rhs) match {
+    case (v, VDouble(d)) => concat(v, VString(doubleToString(d)))
+    case (VDouble(d), v) => concat(VString(doubleToString(d)), v)
+    case (v, VBool(b))   => concat(v, VString(b.toString.toUpperCase))
+    case (VBool(b), v)   => concat(VString(b.toString.toUpperCase), v)
+    case (VString(s1), VString(s2)) => VString(s1 + s2)
+    case es => pickError(es, NA())
+  }
+
+  def doubleToString(d: Double) =
+    if (d.ceil == d) d.toInt.toString
+    else d.toString
+
+  def pickError(t: (Value, Value), default: ErrType) = t match {
+    case (e: VErr, _) => e
+    case (_, e: VErr) => e
+    case _ => VErr(default)
   }
 
 }
--- src/test/scala/scalaExcel/formula/EvaluatorTests.scala
+++ src/test/scala/scalaExcel/formula/EvaluatorTests.scala
@@ -15,7 +15,26 @@ class EvaluatorTests {
   def teval(e: Value, v: String) = assertEquals(e, eval(p parsing v))
 
   @Test def evalConst = teval(VDouble(10), Const(tv(10)))
+
   @Test def binOpSum = teval(VDouble(5), "=2 + 3")
+  @Test def binOpSumString = teval(VErr(NotNumeric()), "=2 + \"a\"")
+  @Test def binOpSumBool = teval(VErr(NotNumeric()), "=2 + TRUE")
+
+  @Test def binOpMul = teval(VDouble(6), "=2 * 3")
+  @Test def binOpMulString = teval(VErr(NotNumeric()), "=2 * \"a\"")
+  @Test def binOpMulBool = teval(VErr(NotNumeric()), "=2 * TRUE")
+
+  @Test def binOpConcat = teval(VString("abc"), "=\"ab\"& \"c\"")
+  @Test def binOpConcatDouble = teval(VString("ab1"), "=\"ab\"& 1")
+  @Test def binOpConcatTrue = teval(VString("abTRUE"), "=\"ab\"& TRUE")
+  @Test def binOpConcatFalse = teval(VString("abFALSE"), "=\"ab\"& FALSE")
+
+  @Test def unOpPlus          = teval(VDouble(5), "=+5")
+  @Test def unOpPlusString    = teval(VErr(NotNumeric()), "=+\"A\"")
+  @Test def unOpMin           = teval(VDouble(-5), "=-5")
+  @Test def unOpMinString     = teval(VErr(NotNumeric()), "=-\"A\"")
+  @Test def unOpPercent       = teval(VDouble(0.25), "=25%")
+  @Test def unOpPercentString = teval(VErr(NotNumeric()), "=\"A\"%")
 
 }
 
