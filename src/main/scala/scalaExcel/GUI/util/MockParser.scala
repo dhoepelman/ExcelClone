f0916211067746a646994d3d4a02da882cfe08a7
Arian Stolwijk <stolwijk.arian@gmail.com>
2014-11-03 16:44:28 +0100

    Implement the parser and evaluator in the MockParser.
    - It has a static context for now, with just one value.
    - Also the values should be formatted nicely, so without showing the Value
      constructors.

--- src/main/scala/scalaExcel/GUI/util/MockParser.scala
+++ src/main/scala/scalaExcel/GUI/util/MockParser.scala
@@ -1,13 +1,33 @@
 package scalaExcel.GUI.util
 
+import scalaExcel.formula.Parser;
+import scalaExcel.formula.Evaluator.{eval, Ctx}
+import scalaExcel.formula.{ACell, VDouble}
+
+// TODO this should be renamed, into CellEvaluator or something...
 object MockParser {
-  def parse(expr: String): (List[(Int, Int)], (List[Any]) => Any) = {
-    //TODO actual parsing
-    val refList = if (expr == "Cell22") List((0, 0), (1, 0), (0, 1)) else List()
-    (refList, {
-      (values: List[Any]) => {
-        if (values == List()) expr else values.mkString("+")
-      }
+
+  val p = new Parser();
+
+  def parse(formula: String): (List[(Int, Int)], (List[Any]) => Any) = {
+
+    val expr = p.parse(formula) match {
+      case p.Success (t, _) => t
+      // this should show some nice dialog that the user did something wrong
+      case p.NoSuccess(msg, _) => throw new IllegalArgumentException(s"Unable to parse $formula: $msg")
+    }
+
+    // refList should come from the AST
+    val refList = if (formula == "Cell22") List((0, 0), (1, 0), (0, 1)) else List()
+
+    // return a tuple, with references and a function that evaluates the
+    // expression when the values are known
+    (refList, (values: List[Any]) => {
+      // values should become the Context eventually
+      val ctx: Ctx = Map(
+        ACell("A", 1) -> VDouble(5)
+      )
+      eval(ctx, expr)
     })
   }
 }
