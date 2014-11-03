7f431d2751efe4ffe79e43fa78aa0d67ff5af8f2
Chris Froussios <darkmatter.tm@gmail.com>
2014-11-03 14:35:54 +0100

    Cell selection as stream of events

--- src/main/scala/scalaExcel/GUI/view/ViewManager.scala
+++ src/main/scala/scalaExcel/GUI/view/ViewManager.scala
@@ -2,15 +2,19 @@ package scalaExcel.GUI.view
 
 import java.net.URL
 import java.util
+import javafx.scene.{input => jfxsi}
 import javafx.scene.{control => jfxsc}
 import javafx.scene.{layout => jfxsl}
 import javafx.{event => jfxe, fxml => jfxf}
+import rx.lang.scala._
+
 import scalafx.scene.layout.AnchorPane
 import scalaExcel.GUI.controller.Mediator
 import scalaExcel.GUI.model.SheetCell
 import scalafx.collections.ObservableBuffer
-import scalafx.scene.control.{TableView, TextField, SelectionMode}
+import scalafx.scene.control._
 import scalaExcel.GUI.model.DataModelFactory.DataRow
+
 
 
 class ViewManager extends jfxf.Initializable {
@@ -29,6 +33,10 @@ class ViewManager extends jfxf.Initializable {
   private var formulaEditor: TextField = _
 
   @jfxf.FXML
+  private var testButtonDelegate: jfxsc.Button = _
+  private var testButton: Button = _
+
+  @jfxf.FXML
   private def makeResizable(event: jfxe.ActionEvent) {
     //make region resizable
   }
@@ -41,8 +49,16 @@ class ViewManager extends jfxf.Initializable {
   }
 
   def initialize(url: URL, rb: util.ResourceBundle) {
+
+    assert(tableContainerDelegate != null)
+    assert(formulaEditorDelegate != null)
+    assert(testButtonDelegate != null)
     tableContainer = new AnchorPane(tableContainerDelegate)
     formulaEditor = new TextField(formulaEditorDelegate)
+    testButton = new Button(testButtonDelegate)
+    assert(tableContainer != null)
+    assert(formulaEditor != null)
+    assert(testButton != null)
 
     table = TableViewBuilder.build(null, null, Mediator.dataTable)
     AnchorPane.setAnchors(table, 0, 0, 0, 0)
@@ -56,14 +72,26 @@ class ViewManager extends jfxf.Initializable {
     selectionModel.setCellSelectionEnabled(true)
     selectionModel.setSelectionMode(SelectionMode.MULTIPLE)
 
-    // Display selected cell onto the formula editor
+    // Test button click stream
+    val obs = Observable.create[Unit](o => new Subscription {
+      testButton.setOnMouseClicked(new jfxe.EventHandler[jfxsi.MouseEvent] {
+        def handle(mouseEvent: jfxsi.MouseEvent): Unit = {
+          o.onNext()
+        }
+      })
+    })
+    obs.subscribe(x => println("Test button clicked"))
+
+    // Create cell selection stream
     val selectedCells = new ObservableBuffer(selectionModel.getSelectedCells)
-    selectedCells.onChange(
-      (source, changes) => {
-        source.take(1).foreach(x =>
-          changeEditorText(Mediator.getCell(x.getRow, x.getColumn).exprString))
-      }
-    )
+    val selectionStream = Observable.create[(Int, Int)](o => new Subscription {
+      selectedCells.onChange((source, changes) => {
+        o.onNext((source.map(x => (x.getRow, x.getColumn)).head))
+      })
+    })
+
+    // Update formula editor when selection changes
+    selectionStream.subscribe(x => changeEditorText(Mediator.getCell(x._1, x._2).exprString))
   }
 
   def changeEditorText(text: String) = formulaEditor.setText(text)
