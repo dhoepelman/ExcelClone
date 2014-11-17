b121cd35e634c1fde2404727ebb4d6cee803e72c
David Hoepelman <dhoepelman@gmail.com>
2014-11-17 12:30:34 +0100

    Hooked up evaluator and parser

--- src/main/scala/scalaExcel/model/Cell.scala
+++ src/main/scala/scalaExcel/model/Cell.scala
@@ -1,10 +1,11 @@
 package scalaExcel.model
 
-import scalaExcel.formula.{VDouble, Value}
+import scalaExcel.formula.{ACell, Value, Parser, VDouble}
 
 // This is a cell object, it can execute it self and find the references inside
 // the formula. This implements a dummy parser/executor
 class Cell(val x: Int, val y: Int, val f: String) {
+  lazy val AST = Cell.parser.parsing(f)
 
   /*
    val sumInt = """=(\d+)\+(\d+)""".r
@@ -32,13 +33,24 @@ class Cell(val x: Int, val y: Int, val f: String) {
   val position = (x, y)
 
    // Add all numbers and references
-   def exec(deps: Map[(Int, Int), Value]) : Value = ???
-   /*refs.foldLeft(lhs + rhs)((sum, ref) => deps get ref match {
-     case Some(x) => x match {
-       case VDouble(d) => VDouble(sum + d)
-       case _ => ???
-     }
-     case None => throw new Exception(s"I, ($x,$y), am trying to use a non existing $ref")
-   })*/
+   def eval(deps: Map[(Int, Int), Value]) : Value = Cell.evaluator.eval(Cell.Ctx(deps), AST)
 
- }
+}
+
+object Cell {
+  val parser = new Parser()
+  val evaluator = scalaExcel.formula.Evaluator
+
+  def Ctx(values: Map[(Int, Int), Value])(c : ACell) = values get (colToNum(c.c), c.r) match {
+    case Some(v) => v
+    case None    => throw new IllegalArgumentException(s"Cell (${c.c},${c.r}}) not found in Ctx")
+  }
+
+  // A => 1, B => 2, AA => 27
+  def colToNum(r: String): Int = r.foldLeft(0)(_ * 26 + _ - 64)
+
+  // 1 => A, B => 2, 27 => AA
+  def numToCol(r: Int): String =
+    if (r >= 1) numToCol((r - 1) / 26) + ((r - 1) % 26 + 65).toChar
+    else ""
+}
