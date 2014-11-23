d017206996f7f4b1629ae7773736ff8f9a33eaa6
Maria Voinea <M.Voinea@student.tudelft.nl>
2014-11-23 09:49:55 +0100

    fix duplicate from merge

--- src/main/scala/scalaExcel/model/ModelMutations.scala
+++ src/main/scala/scalaExcel/model/ModelMutations.scala
@@ -6,4 +6,3 @@ package scalaExcel.model
 abstract class ModelMutations
 case class Refresh() extends ModelMutations
 case class SetCell(x: Int, y: Int, f: String) extends ModelMutations
-case class Refresh() extends ModelMutations
