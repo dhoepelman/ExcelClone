ee6873c1c7a6ce5fc3e85dc4f916c72575ad7fb0
Maria Voinea <M.Voinea@student.tudelft.nl>
2014-11-25 00:25:00 +0100

    column translator modified for 0-based references

--- src/main/scala/scalaExcel/util/ColumnTranslator.scala
+++ src/main/scala/scalaExcel/util/ColumnTranslator.scala
@@ -1,11 +1,12 @@
 package scalaExcel.util
 
 object ColumnTranslator {
-  // A => 1, B => 2, AA => 27
-  def colToNum(r: String): Int = r.foldLeft(0)(_ * 26 + _ - 64)
+  // A => 0, B => 1, AA => 26
+  def colToNum(r: String): Int = r.foldLeft(-1)(_ * 26 + _ - 39)
 
-  // 1 => A, B => 2, 27 => AA
+  // 0 => A, 1 => B, 26 => AA
   def numToCol(r: Int): String =
-    if (r >= 1) numToCol((r - 1) / 26) + ((r - 1) % 26 + 65).toChar
+    if (r >= 1) numToCol(r / 26 - 1) + (r % 26 + 65).toChar
+    else if (r == 0) "A"
     else ""
 }
