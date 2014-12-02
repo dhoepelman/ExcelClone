4a76d8ca9a21893da62403f268baeca66c1b618c
Maria Voinea <M.Voinea@student.tudelft.nl>
2014-12-02 13:47:26 +0100

    separate implicit class

--- src/main/scala/scalaExcel/rx/operators/WithLatest.scala
+++ src/main/scala/scalaExcel/rx/operators/WithLatest.scala
@@ -8,13 +8,18 @@ object WithLatest {
   // TODO: Replace this with RxJava's operator if/when it gets available: https://github.com/ReactiveX/RxJava/issues/405
   // Source: http://stackoverflow.com/a/27207073/572635
   implicit class RxWithLatest[B](slow: Observable[B]) {
-    def withLatest[A](fast : Observable[A]): Observable[(A,B)] =
-      fast.map({a => slow.publish.refCount.map({b => (a,b)})}).switch
+    def withLatest[A](fast: Observable[A]): Observable[(A, B)] =
+      fast.map({
+        a => slow.publish.refCount.map({
+          b => (a, b)
+        })
+      }).switch
+  }
 
-    def distinctWithAllLatest[A](fast : Observable[List[A]]): Observable[(A,B)] =
-      withLatest(fast)
+  implicit class RxDistinctWithAllLatest[B](slow: Observable[B]) {
+    def distinctWithAllLatest[A](fast: Observable[List[A]]): Observable[(A, B)] =
+      slow.withLatest(fast)
         .distinctUntilChanged(_._2)
         .flatMap(x => Observable.from(x._1.map(i => (i, x._2))))
   }
-
 }
