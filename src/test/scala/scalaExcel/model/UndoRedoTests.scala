7d40810cac82ee7fe17a6472ad4f58b8e89f2092
David Hoepelman <dhoepelman@gmail.com>
2014-12-06 12:48:28 +0100

    Fixed UndoRedo tests

--- src/test/scala/scalaExcel/model/UndoRedoTests.scala
+++ src/test/scala/scalaExcel/model/UndoRedoTests.scala
@@ -9,29 +9,29 @@ class UndoRedoTests {
   @Test def undoTest() : Unit = {
     List(4,3,2,1,0).foldLeft(zeroToFive)((undoRedo, expected) => {
       var res = undoRedo.undo()
-      assertEquals(expected, res._2.get)
-      res._1
+      assertEquals(expected, res.current)
+      res
     })
   }
 
   @Test def redoTest() : Unit = {
-    val withUndone = List(4,3,2,1,0).foldLeft(zeroToFive)((undoRedo, expected) => undoRedo.undo()._1)
+    val withUndone = List(4,3,2,1,0).foldLeft(zeroToFive)((undoRedo, expected) => undoRedo.undo())
     List(1,2,3,4).foldLeft(withUndone)((undoRedo, expected) => {
       var res = undoRedo.redo()
-      assertEquals(expected, res._2.get)
-      res._1
+      assertEquals(expected, res.current)
+      res
     })
   }
 
   @Test def undoAndRedoTest() : Unit = {
     val withUndone = List(4,3,2)
-      .foldLeft(zeroToFive)((undoRedo, expected) => undoRedo.undo()._1)
+      .foldLeft(zeroToFive)((undoRedo, expected) => undoRedo.undo())
     val withRedo = List(3,4,5)
-      .foldLeft(withUndone)((undoRedo, expected) => undoRedo.redo()._1)
+      .foldLeft(withUndone)((undoRedo, expected) => undoRedo.redo())
     List(4,3,2).foldLeft(withRedo)((undoRedo, expected) => {
       var res = undoRedo.undo()
-      assertEquals(expected, res._2.get)
-      res._1
+      assertEquals(expected, res.current)
+      res
     })
   }
 }
