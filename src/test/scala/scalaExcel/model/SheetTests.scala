e6123f5181fe21b690525723c60433b30f016b4b
Arian Stolwijk <stolwijk.arian@gmail.com>
2014-12-11 18:11:02 +0100

    Simplify the CopyPastTests as they can be moved in the SheetTests

--- src/test/scala/scalaExcel/model/SheetTests.scala
+++ src/test/scala/scalaExcel/model/SheetTests.scala
@@ -37,13 +37,41 @@ class SheetTests {
     assertEquals((0, 0), sheet.size)
   }
 
-  @Test def copyCell() = {
+  @Test def copyCell1() = {
     val sheet = new Sheet()
       .setCell((0, 0), "1")
-      .copyCell((0, 0), (1, 0))
-    assertEquals((2, 1), sheet.size)
+      .setCell((1, 0), "2")
+      .setCell((0, 1), "=A1+1")
+      .copyCell((0, 1), (1, 1))
+    assertEquals((2, 2), sheet.size)
     assertEquals(VDouble(1), sheet.getValue((0, 0)))
-    assertEquals(VDouble(1), sheet.getValue((1, 0)))
+    assertEquals(VDouble(2), sheet.getValue((1, 0)))
+    assertEquals(VDouble(3), sheet.getValue((1, 1)))
+  }
+
+  @Test def cutCell1() = {
+    val sheet = new Sheet()
+      .setCell((0, 0), "1")
+      .setCell((1, 0), "2")
+      .setCell((0, 1), "=A1+1")
+      .cutCell((0,1),(1,1))
+    assertEquals(VDouble(2), sheet.getValue((1, 1)))
+  }
+
+  @Test def cutCell2() = {
+    val sheet = new Sheet()
+      .setCell((0, 0), "1")
+      .setCell((1, 0), "2")
+      .setCell((0, 1), "=A1+1")
+      .setCell((1, 1), "=$A$1+1")
+      .setCell((3, 0), "=C1+0.5")
+    assertEquals(VDouble(0.5), sheet.getValue((3, 0)))
+
+    val sheet2 = sheet.cutCell((0, 0), (2, 0))
+
+    assertEquals(VDouble(2), sheet2.getValue((0, 1)))
+    assertEquals(VDouble(2), sheet2.getValue((1, 1)))
+    assertEquals(VDouble(1.5), sheet2.getValue((3, 0)))
   }
 
   @Test def updateStyle() = {
