93860ad6b3a1dadae502accbd7d8b046c22875f5
Arian Stolwijk <stolwijk.arian@gmail.com>
2014-12-15 12:08:39 +0100

    Fixes code alignment in the ReferenceFinder

--- src/main/scala/scalaExcel/formula/ReferenceFinder.scala
+++ src/main/scala/scalaExcel/formula/ReferenceFinder.scala
@@ -9,25 +9,25 @@ object ReferenceFinder {
   case object Empty extends Node
 
   def findRefCells(e: Expr, l: List[ACell] = List()): List[ACell] = getChildren(e) match {
-    case Empty     => l
+    case Empty      => l
     case Leaf(e2)   => l.+:(e2)
-    case Leafs(ls) => l ++ (ls map {
+    case Leafs(ls)  => l ++ (ls map {
       case Leaf(e2) => e2
     })
-    case Branch(b) => l ++ (b map (findRefCells(_))).flatten
+    case Branch(b)  => l ++ (b map (findRefCells(_))).flatten
   }
 
   // get all child nodes
   private def getChildren(e: Expr) = e match {
-    case BinOp(_, l, r)       => Branch(List(l, r))
+    case BinOp(_, l, r)        => Branch(List(l, r))
     case UnOp(_, e2)           => Branch(List(e2))
-    case Call(_, args)        => Branch(args)
+    case Call(_, args)         => Branch(args)
     case SheetReference(_, e2) => Branch(List(e2))
     case Group(e2)             => Branch(List(e2))
-    case e: ACell             => Leaf(e)
-    case c: Cell              => desugarCell(c)
-    case r: Range             => desugarRange(r)
-    case _                    => Empty
+    case e: ACell              => Leaf(e)
+    case c: Cell               => desugarCell(c)
+    case r: Range              => desugarRange(r)
+    case _                     => Empty
   }
 
   private def desugarCell(e: Expr) = e match {
